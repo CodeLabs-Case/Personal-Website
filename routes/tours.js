9b29db3362c833c09e6faa7bb06258cd5dfb1506
blackBoxMod <davis.gamer07@gmail.com>
2020-11-12 19:42:08 -0500

    Testing

--- routes/tours.js
+++ routes/tours.js
@@ -24,39 +24,45 @@ router.get('/:id', (req, res, err)=>{
 
     let param = req.params.id
     let id = parseInt(param, 10)
-
     
+
     
     // If you are able to do store and operate on the data in this way then it will be the better option.
     // You will want to make another project where you connect to the mongodb instance so that you know how to ...
     // ... when you are working with Elastic.
     var contents = fs.readFileSync('/var/app/current/models/database.json');
     var jsonContent = JSON.parse(contents);
-    res.send(jsonContent.tours[id])
 
-    // Tours.find()
-    // .then(tours => res.json(tours))
-    // .catch(err => res.status(400).json('Error: ' + err))
-})
-
-router.get('/all', (req, res, err)=>{
-    if(err){
-        console.log(err)
+    if(id == 'all'){
+        res.send(jsonContent)
+    } else {
+        res.send(jsonContent.tours[id])
     }
     
-    
-    
-    // If you are able to do store and operate on the data in this way then it will be the better option.
-    // You will want to make another project where you connect to the mongodb instance so that you know how to ...
-    // ... when you are working with Elastic.
-    var contents = fs.readFileSync('/var/app/current/models/database.json');
-    var jsonContent = JSON.parse(contents);
-    res.send(jsonContent)
 
     // Tours.find()
     // .then(tours => res.json(tours))
     // .catch(err => res.status(400).json('Error: ' + err))
 })
+
+// router.get('/all', (req, res, err)=>{
+//     if(err){
+//         console.log(err)
+//     }
+    
+    
+    
+//     // If you are able to do store and operate on the data in this way then it will be the better option.
+//     // You will want to make another project where you connect to the mongodb instance so that you know how to ...
+//     // ... when you are working with Elastic.
+//     var contents = fs.readFileSync('/var/app/current/models/database.json');
+//     var jsonContent = JSON.parse(contents);
+//     res.send(jsonContent)
+
+//     // Tours.find()
+//     // .then(tours => res.json(tours))
+//     // .catch(err => res.status(400).json('Error: ' + err))
+// })
 
 
 
