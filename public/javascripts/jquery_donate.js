8601bb86114a49a4308cfbeef08f6bf3b1a37bce
blackBoxMod <davis.gamer07@gmail.com>
2020-11-23 10:14:47 -0500

    Testing Connections

--- public/javascripts/jquery_donate.js
+++ public/javascripts/jquery_donate.js
@@ -1,3 +1,20 @@
 $(document).ready(()=>{
+    var stripeHandler = StripeCheckout.configure({
+        key: stripePublicKey,
+        locale: auto,
+        token: function(token){
+            // Calls this after Stripe API call is done
+        }
+    })
     
+    function donateClickedFiveDollar(){
+        var donate = 5000
+        stripeHandler.open({
+            amount: donate
+        })
+    }
+
+    $('.button-donate-five').on('click', ()=>{
+        donateClickedFiveDollar()
+    })
 })
